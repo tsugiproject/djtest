5cabd5d0b4b4e5de4d05a2d600a7654f3d79e806
Charles Severance <csev@umich.edu>
2019-04-15 09:28:47 +0200

    Rename

--- djtest/settings.py
+++ djtest/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'launch.apps.LaunchConfig',
+    'grade.apps.GradeConfig',
 ]
 
 MIDDLEWARE = [
--- launch/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LaunchConfig(AppConfig):
-    name = 'launch'
