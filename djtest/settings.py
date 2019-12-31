2c7cceb0bc609f22f17b7d6dc9f6348b61ae6432
Charles Severance <csev@umich.edu>
2019-12-31 10:01:40 -0500

    Move to django 3.0.1

--- djtest/settings.py
+++ djtest/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    # Must remove for Tsugi
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
